91618a7a3b8109aaf14712d69a01f9ddede07dd5
Mohamed Amer <m.amer_abutlabe@hotmail.com>
2022-05-31 08:13:18 +0200

    functionality is finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './Home';
+import Movie from './SingleMovie';
 function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+	return (
+		<Routes>
+			<Route path='/' element={<Home />} />
+			<Route path='/movies/:id' element={<Movie />} />
+		</Routes>
+	);
 }
 
 export default App;
